bcfffce8ea46c87245f1bf6520df156f601b48d6
holomanga <holomanga@gmail.com>
2020-12-06 23:44:22 +0000

    I'm going to make this general, I promise

--- EffectiveAltruism/animalcharityevaluators.py
+++ EffectiveAltruism/animalcharityevaluators.py
@@ -1,6 +1,5 @@
 import numpy as np
 import csv
-from enum import Enum
 
 # Markov chain model of ACE top charities
 
@@ -13,15 +12,15 @@ with open('EffectiveAltruism\\ace_statuses.csv', newline='') as csvfile:
 charities = charities[1:]
 
 reviewStatusTypes = {
-    "unreviewed": "null",
-    "exploratory": "null",
+    "unreviewed": "unreviewed",
+    "exploratory": "unreviewed",
     "comprehensive": "comprehensive",
     "standout": "standout",
     "top": "top"
 }
 
 transitions = {
-	"null": [],
+	"unreviewed": [],
 	"comprehensive": [],
 	"standout": [],
 	"top": []
@@ -29,8 +28,9 @@ transitions = {
 
 # For each pair of years, append the transition to the appropriate transitions dictionary
 for charity in charities:
-	for year in range(len(charity)-1):
-		transitions[reviewStatusTypes[charity[year]]].append(reviewStatusTypes[charity[year+1]])
+  for year in range(len(charity)-1):
+      if reviewStatusTypes[charity[year+1]] != "unreviewed":
+        transitions[reviewStatusTypes[charity[year]]].append(reviewStatusTypes[charity[year+1]])
 
 # Now calculate each transition as a % of total transitions and print the values
 uniqueTop, countsTop = np.unique(transitions["top"], return_counts=True)
@@ -39,7 +39,21 @@ uniqueStandout, countsStandout = np.unique(transitions["standout"], return_count
 standoutTransitions =  dict(zip(uniqueStandout,countsStandout/sum(countsStandout)))
 uniqueComprehensive, countsComprehensive = np.unique(transitions["comprehensive"], return_counts=True)
 comprehensiveTransitions = dict(zip(uniqueComprehensive,countsComprehensive/sum(countsComprehensive)))
-uniqueNull, countsNull = np.unique(transitions["null"], return_counts=True)
+uniqueNull, countsNull = np.unique(transitions["unreviewed"], return_counts=True)
 nullTransitions = dict(zip(uniqueNull,countsNull/sum(countsNull)))
 
+print("Top to " + str(topTransitions))
+print("Standout to " + str(standoutTransitions))
+print("Comprehensive to " + str(comprehensiveTransitions))
+print("Unreviewed to " + str(nullTransitions))
+
+# Now we can do the markov chain model
+from scipy.stats import poisson
+from np.random import rand
+
+# currentCharityStatuses: the status of each WAS charity ACE evaluates
+currentCharityStatuses = ["top"]
+
 totalNumberOfCharities = len(charities)
+percentageCharitiesOfDesiredType = (len(currentCharityStatuses)+1)/(totalNumberOfCharities+2)
+newCharitiesPerYear = len(charities)/(len(charities[0])-1) * percentageCharitiesOfDesiredType
