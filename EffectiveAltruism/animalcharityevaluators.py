d886c83ee6b32be3a107f5921277416b67f1e0b4
holomanga <holomanga@gmail.com>
2020-12-06 23:58:14 +0000

    Do everything with lists not dicts (god I love how weak these types are)

--- EffectiveAltruism/animalcharityevaluators.py
+++ EffectiveAltruism/animalcharityevaluators.py
@@ -11,36 +11,33 @@ with open('EffectiveAltruism\\ace_statuses.csv', newline='') as csvfile:
         charities.append(row[1:])
 charities = charities[1:]
 
-reviewStatusTypes = {
-    "unreviewed": "unreviewed",
-    "exploratory": "unreviewed",
-    "comprehensive": "comprehensive",
-    "standout": "standout",
-    "top": "top"
+reviewStatusCodes = {
+    "unreviewed": 0,
+    "exploratory": 0,
+    "comprehensive": 1,
+    "standout": 2,
+    "top": 3
 }
 
-transitions = {
-	"unreviewed": [],
-	"comprehensive": [],
-	"standout": [],
-	"top": []
-}
+transitions = [[],[],[],[]]
 
 # For each pair of years, append the transition to the appropriate transitions dictionary
 for charity in charities:
   for year in range(len(charity)-1):
-      if reviewStatusTypes[charity[year+1]] != "unreviewed":
-        transitions[reviewStatusTypes[charity[year]]].append(reviewStatusTypes[charity[year+1]])
+      if reviewStatusCodes[charity[year+1]] != 0:
+        transitions[reviewStatusCodes[charity[year]]].append(reviewStatusCodes[charity[year+1]])
+
+# TODO: I want a list for which list[x][y] is the probability of transition from x to y
 
 # Now calculate each transition as a % of total transitions and print the values
-uniqueTop, countsTop = np.unique(transitions["top"], return_counts=True)
-topTransitions = dict(zip(uniqueTop,countsTop/sum(countsTop)))
-uniqueStandout, countsStandout = np.unique(transitions["standout"], return_counts=True)
-standoutTransitions =  dict(zip(uniqueStandout,countsStandout/sum(countsStandout)))
-uniqueComprehensive, countsComprehensive = np.unique(transitions["comprehensive"], return_counts=True)
-comprehensiveTransitions = dict(zip(uniqueComprehensive,countsComprehensive/sum(countsComprehensive)))
-uniqueNull, countsNull = np.unique(transitions["unreviewed"], return_counts=True)
-nullTransitions = dict(zip(uniqueNull,countsNull/sum(countsNull)))
+uniqueTop, countsTop = np.unique(transitions[3], return_counts=True)
+topTransitions = list(zip(uniqueTop,countsTop/sum(countsTop)))
+uniqueStandout, countsStandout = np.unique(transitions[2], return_counts=True)
+standoutTransitions = list(zip(uniqueStandout,countsStandout/sum(countsStandout)))
+uniqueComprehensive, countsComprehensive = np.unique(transitions[1], return_counts=True)
+comprehensiveTransitions = list(zip(uniqueComprehensive,countsComprehensive/sum(countsComprehensive)))
+uniqueNull, countsNull = np.unique(transitions[0], return_counts=True)
+nullTransitions = list(zip(uniqueNull,countsNull/sum(countsNull)))
 
 print("Top to " + str(topTransitions))
 print("Standout to " + str(standoutTransitions))
@@ -49,7 +46,6 @@ print("Unreviewed to " + str(nullTransitions))
 
 # Now we can do the markov chain model
 from scipy.stats import poisson
-from np.random import rand
 
 # currentCharityStatuses: the status of each WAS charity ACE evaluates
 currentCharityStatuses = ["top"]
